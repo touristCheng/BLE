553405aa5c35209be105c6c50dd4e8a538b8235d
touristCheng <acccheng@icloud.com>
2016-01-07 15:46:24 +0800

    Initial Commit

---
+++ NewBle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewBle
+//
+//  Created by chengshuo on 16/1/7.
+//  Copyright © 2016年 chengshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewBle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewBle
+//
+//  Created by chengshuo on 16/1/7.
+//  Copyright © 2016年 chengshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
